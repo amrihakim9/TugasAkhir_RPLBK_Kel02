8f04cbcb132220de9c58ff4eb5dc733839bed6e6
amrihakim9 <64180632+amrihakim9@users.noreply.github.com>
2021-10-23 08:46:28 +0700

    Update NavbarComponent.js

--- kedaiphone/src/components/NavbarComponent.js
+++ kedaiphone/src/components/NavbarComponent.js
@@ -4,7 +4,9 @@ import {Nav, Navbar, NavDropdown, Container } from 'react-bootstrap'
 const NavbarComponent = () => {
   return (
     <Navbar className="justify-content-center" variant="dark" expand="lg">
+      <Container>
       <Navbar.Brand href="#home"><strong>Kedai</strong> Phone</Navbar.Brand>
+      </Container>
     </Navbar>
   );
 };
